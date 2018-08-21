fb84af91417b138cdecebd697655aba502da7e8c
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-08-21 12:29:45 +0300

    Add test_running for UNLOGGED tables

---
+++ test_running.unlogged.pg.sql
@@ -0,0 +1,14 @@
+\timing on
+
+DROP TABLE test_pgsphere;
+
+-- Tables
+CREATE UNLOGGED TABLE test_pgsphere (id INT, point spoint);
+
+-- -- Random data
+-- INSERT INTO test_pgsphere (id, point) (SELECT generate_series(1,1000000), spoint(random()*2*pi(), acos(1-2*random())-pi()/2));
+
+-- Indices
+CREATE INDEX test_pgsphere_idx ON test_pgsphere USING gist(point);
+
+VACUUM ANALYZE;
---
+++ test_running.unlogged.postgis.sql
@@ -0,0 +1,14 @@
+\timing on
+
+DROP TABLE test_postgis;
+
+-- Tables
+CREATE UNLOGGED TABLE test_postgis (id INT, point geography(POINT, 4326));
+
+-- -- Random data
+-- INSERT INTO test_postgis (id, point) (SELECT generate_series(1,1000000), st_geographyfromtext(concat('POINT(', random()*360-180, ' ', acos(1-2*random())/pi()*180-90, ')')));
+
+-- Indices
+CREATE INDEX test_postgis_idx ON test_postgis USING gist(point);
+
+VACUUM ANALYZE;
